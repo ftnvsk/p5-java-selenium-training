c01fdba0bdec46a99c1c8b80615d95572ea2e34c
Filip Tanevski <filip_tanata@yahoo.com>
2022-11-29 08:51:07 +0200

    ss

--- src/test/java/HelpMethods/ElementMethods.java
+++ src/test/java/HelpMethods/ElementMethods.java
@@ -9,7 +9,6 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-
 import java.time.Duration;
 
 public class ElementMethods {
